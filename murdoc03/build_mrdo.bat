e105f4a0d7c9ae4184b1fd3ca1839f5da5333e7e
Patrick Moore <patrickjamesmoore@gmail.com>
2022-05-18 21:35:37 +0100

    reinstated Murdoc splash screen

--- murdoc03/build_mrdo.bat
+++ murdoc03/build_mrdo.bat
@@ -1,1 +1,1 @@
-copy /b murdoc.gba+header.dat+a4-01.bin+c4-02.bin+e4-03.bin+f4-04.bin+s8-09.bin+u8-10.bin+r8-08.bin+n8-07.bin+h5-05.bin+k5-06.bin+u02--2.bin+t02--3.bin+f10--1.bin+j10--4.bin mrdo.gba
+copy /b murdoc.gba+murdoc.raw+header.dat+a4-01.bin+c4-02.bin+e4-03.bin+f4-04.bin+s8-09.bin+u8-10.bin+r8-08.bin+n8-07.bin+h5-05.bin+k5-06.bin+u02--2.bin+t02--3.bin+f10--1.bin+j10--4.bin mrdo.gba
--- murdoc03/build_yankeedo.sh
+++ murdoc03/build_yankeedo.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-cat murdoc.gba header6.dat a4-01.bin yd_d2.c4 e4-03.bin f4-04.bin s8-09.bin u8-10.bin r8-08.bin n8-07.bin yd_d5.h5 yd_d6.k5 u02--2.bin t02--3.bin f10--1.bin j10--4.bin > mrdo.gba
+cat murdoc.gba murdoc.raw header6.dat a4-01.bin yd_d2.c4 e4-03.bin f4-04.bin s8-09.bin u8-10.bin r8-08.bin n8-07.bin yd_d5.h5 yd_d6.k5 u02--2.bin t02--3.bin f10--1.bin j10--4.bin > mrdo.gba
