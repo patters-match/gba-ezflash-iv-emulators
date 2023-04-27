5f64ba98b6e5a7ba7c9e7235411940b717bdc30e
patters-match <55089098+patters-match@users.noreply.github.com>
2023-04-27 01:28:55 +0100

    Update build.sh

--- hvca140/build.sh
+++ hvca140/build.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+# Place disksys.rom in the same folder as this script
+
 for file in add/*.fds add/*.nes ; do
 	out="${file/ \(*}" # strip brackets from name
 	if [ -e "${file%.*}.cfg" ]; then
